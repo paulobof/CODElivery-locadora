ad9830a49dfd6e62f1fbc80a8390d88d6528f407
ÍCARO SANTOS <121477907+FlightofIcarus@users.noreply.github.com>
2023-11-24 16:56:21 -0300

    feat: Criação métodos de Agencia
    Criação dos métodos verificarClienteCadastrado, que verifica se o cliente já está no array alugando algum veículo, cadastrarCliente, que cadastra o cliente no array de clientes e o deletarCliente, que retira o cliente do array de clientes.

--- src/models/Agencia.ts
+++ src/models/Agencia.ts
@@ -22,7 +22,20 @@ export default class Agencia {
   // Metodo alugar foi movido para Cliente.ts
   // alugar() {}
 
-  devolver(cpf: string) {}
+  static verificarClienteCadastrado(cpfcliente: string){
+    return this.clientes.some((cliente) => cliente.cpf === cpfcliente)
+  }
+    
+  
+  static cadastrarCliente({nomeDoCliente, cpfDoCliente, cnhDoCliente}: Cliente){
+    if(!this.verificarClienteCadastrado(cpfDoCliente))
+    this.clientes.push({nome: nomeDoCliente, cpf: cpfDoCliente, cnh: cnhDoCliente})
+  }
+
+  static deletarCliente(cpfcliente: string){
+    if(this.verificarClienteCadastrado(cpfcliente)){
+    this.clientes.splice(clientes.findIndex(cliente => cliente.cpf === cpfcliente), 1)} return
+  }
 
   static listarDisponiveis() {
     const veiculosDisponiveis = Agencia.veiculos.filter(
